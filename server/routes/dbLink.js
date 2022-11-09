896b6806be1687340eff3ff4ace6716b164a3d75
Cavin <sanghpark1@gmail.com>
2022-11-09 11:38:05 -0500

    foreign keys controller created

--- server/routes/dbLink.js
+++ server/routes/dbLink.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const dbLinkController = require('../controllers/dbLinkController');
+const fnKeyController = require('../controllers/fnKeyController');
 
-router.post('/', dbLinkController.connectDb, dbLinkController.test, (req,res) => {
-    return res.status(202).json(res.locals.fK)
+router.post('/', dbLinkController.connectDb, dbLinkController.extractFnKeys, fnKeyController.parseFnKeyData, (req,res) => {
+    return res.status(202).json('successly parsed database data!')
 })
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const dbLinkRouter = require('./routes/dbLink');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.resolve(__dirname, '../client')));
+
 // send database link to appropriate router
 app.use('/db', dbLinkRouter);
 
@@ -28,8 +30,6 @@ app.use((err, req, res, next) => {
     console.log(errorObj.log);
     return res.status(errorObj.status).json(errorObj.message);
 })
-
-app.use(express.static(path.resolve(__dirname, '../client')));
 
 app.listen(port, () => console.log('server listening on port ' + port));
 module.exports = app;
