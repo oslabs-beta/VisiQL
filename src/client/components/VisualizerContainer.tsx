4b3c79e1b5eeaeb695aeb1ea7782c1b8b3195b06
Jordan Jeter <jordan@Jordans-MBP.lan>
2022-11-14 22:32:36 -0500

    make tree collapsable, clickable

--- src/client/components/VisualizerContainer.tsx
+++ src/client/components/VisualizerContainer.tsx
@@ -1,18 +1,22 @@
 import React, { useState, useEffect, useRef } from 'react';
 //@ts-ignore
-import Diagram from './Diagram';
-import TestDiagram from './testDiagram';
+// import TestDiagram from './TestDiagram';
+// import Diagram from './Diagram';
+// import TreeTest from './TreeTest';
+import Copy from './Copy';
 
 
 const VisualizerContainer = (props: { data: object; }) => {
   
   return (
     <div className='schema-vis-container'>
-      <Diagram data={props.data} />
-      {/* <TestDiagram data={props.data}/> */}
+      <Copy data={props.data}/>
+      {/* <Diagram data={props.data} /> */}
+      {/* <TreeTest data={props.data} /> */}
     </div>
   );
 };
 
 export default VisualizerContainer;
 //might need the fetch request and onSubmit here to trigger the update to data
+// 
